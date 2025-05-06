1b1dba99002029b5cbc0f1dd940019b4db3c9581
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2025-05-07 00:23:18 +0300

    muutused enne algversiooni esitust

--- project/ascii_art.cpp
+++ project/ascii_art.cpp
@@ -5,9 +5,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include <filesystem>
-
 #include "stb_image_resize2.h"
+#include <filesystem>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -34,29 +33,32 @@ int main() {
     cout << "Palun sisesta failinimi koos failimuutujaga): ";
     cin >> in;
 
-    // Check if file exists
-    if (!filesystem::exists(in)) {
-        cerr << "Faili ei leitud: " << in << endl;
+    // Kontrollib kas faili on olemas
+    ifstream file;
+    file.open(in);
+    if (!file) {
+        cerr << "Faili " << in << " ei leitud. :/" << endl;
         return 1;
     }
+    file.close();
 
     // Laeb pikslite andmed ühte muutujasse
     unsigned char* data = stbi_load(in.c_str(), &width, &height, &channels, 3);
 
     if (!data) {
-        cerr << "Vigane sisend." << endl;
+        cerr << "Vigane sisend. Kas fail on pilt?" << endl;
         return 1;
     }
 
-    //laiuse küsimine
-    cout << "Sisesta soovitud laiuse väärtus : ";
+    // Laiuse küsimine
+    cout << "Sisesta soovitud laiuse väärtus (vaikimisi 540): ";
     cin >> out_width;
-    if (out_width <= 0) {
+    if (out_width <= 0 || !cin) {
         out_width = 540;
-        cout << "Vale sisestus. Kasutan vaikeväärtust: " << out_width << "\n";
+        cout << "Vale sisestus. Kasutan vaikeväärtust " << out_width << "\n";
     }
 
-    int out_height = (height * out_width) / width / 2; // kõrgus on siin dünaamiline vastavalt pildile
+    int out_height = (height * out_width) / width / 2; // Kõrgus on siin dünaamiline vastavalt pildile
 
     // Kuna ASCII sümbolid pole ruudud (nagu on piksel), tuleb pikslite andmed muuta väiksemaks stb resize meetodiga,
     // vastasel juhul vaikeparameetriga ilmuksid sümbolid vertikaalselt teineteise peal ning ASCII kunsti poleks näha
@@ -64,11 +66,11 @@ int main() {
     stbir_resize_uint8_srgb(data, width, height, 0, resized, out_width, out_height, 0, STBIR_RGB);
     stbi_image_free(data);
 
-    // soovitud output faili nime küsimine
-    cout << "Soovid määrata väljundfaili nime? (vajuta Enter vahelejätmiseks): ";
-    cin.ignore();
-    getline(cin, out_filename);
+    // Output faili nime küsimine, jätab samaks mis sisend kui jäetakse vahele
+    cout << "Sisesta väljundfaili nimi: ";
+    cin >> out_filename;
 
+    // Lisab failile .txt failimuutuja
     if (out_filename.empty()) {
         size_t dotPos = in.find_last_of('.');
         out_filename = (dotPos != string::npos ? in.substr(0, dotPos) : in) + ".txt";
@@ -76,6 +78,7 @@ int main() {
         out_filename += ".txt";
     }
 
+    // Pikslid -> ASCII sümboliteks loogika
     ofstream out(out_filename);
     for (int y = 0; y < out_height; y += 5) {
         for (int x = 0; x < out_width; x += 2) {
@@ -87,7 +90,7 @@ int main() {
     }
     cout << "Programm on töö edukalt lõpetanud! Tulemus on nähtav root kaustas." << endl;
 
-    // Vabastab andmed
+    // Vabastab andmed pärast teisendust
     out.close();
     delete[] resized;
     return 0;
